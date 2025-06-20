f080c0ea61757838d0fafcde001e20380b3c9aab
Jake <jake@Jakes-MacBook-Pro-2.local>
2025-06-20 02:59:27 -0400

    Update cors for Render deployment

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,7 +1,11 @@
 # config/initializers/cors.rb
+
+frontend_url = ENV.fetch("FRONTEND_URL", "http://localhost:3000")
+
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000"
+    origins frontend_url
+
     resource "*",
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
